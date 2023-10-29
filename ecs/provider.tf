ef52916dcd2d4414c0a46fb5627a0073c1dd9903
Amar <amarnath3058@gmail.com>
2023-10-29 12:06:42 +0530

    1st cmt

--- ecs/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  access_key = "AKIAVG5YJAZKVVZIYSGI"
-  secret_key = "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"
-  region     =  "us-east-1"
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
-}
